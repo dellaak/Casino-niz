6111228f886e001f200f241a6afda2ecb630398e
Delshad <Delshadk1@gmail.com>
2019-12-31 02:48:13 +0100

    added header-redirects for netlify and add react-snap for pre render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 import App from "./App/index";
 import 'bootstrap/dist/css/bootstrap.css';
 import "typeface-exo-2"
@@ -10,8 +10,12 @@ import "@openfonts/manjari_malayalam"
 
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+    hydrate(<App />, rootElement);
+} else {
+    render(<App />, rootElement);
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
