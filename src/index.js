2bcdbc1e51c80df8fbafff5de7717332da563492
Delshad <Delshadk1@gmail.com>
2020-01-01 21:30:33 +0100

    remove inlinse reactsnap

--- src/index.js
+++ src/index.js
@@ -13,24 +13,23 @@ import * as serviceWorker from './serviceWorker';
 const rootElement = document.getElementById("root");
 if (rootElement.hasChildNodes()) {
     hydrate(<App />, rootElement);
-    serviceWorker.register({
-        onUpdate: registration => {
-            const waitingServiceWorker = registration.waiting
-
-            if (waitingServiceWorker) {
-                waitingServiceWorker.addEventListener("statechange", event => {
-                    if (event.target.state === "activated") {
-                        window.location.reload()
-                    }
-                });
-                waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
-            }
-        }
-    });
 } else {
     render(<App />, rootElement);
 }
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register({
+    onUpdate: registration => {
+        const waitingServiceWorker = registration.waiting
 
+        if (waitingServiceWorker) {
+            waitingServiceWorker.addEventListener("statechange", event => {
+                if (event.target.state === "activated") {
+                    window.location.reload()
+                }
+            });
+            waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
+        }
+    }
+});
