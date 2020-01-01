f461f8b6808a91db131f0303c59bcda10d5f8f99
Delshad <Delshadk1@gmail.com>
2020-01-01 22:42:36 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { hydrate, render } from "react-dom";
+import { render } from 'react-snapshot';
 import App from "./App/index";
 import 'bootstrap/dist/css/bootstrap.css';
 import "typeface-exo-2"
@@ -10,12 +10,10 @@ import "@openfonts/manjari_malayalam"
 
 import * as serviceWorker from './serviceWorker';
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-    hydrate(<App />, rootElement);
-} else {
-    render(<App />, rootElement);
-}
+render(
+    <App />,
+    document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
