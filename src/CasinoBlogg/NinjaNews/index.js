445552b2261575a48dfb611af6a45254614389ed
Delshad <Delshadk1@gmail.com>
2020-01-29 21:56:39 +0100

    fix

--- src/CasinoBlogg/NinjaNews/index.js
+++ src/CasinoBlogg/NinjaNews/index.js
@@ -17,7 +17,7 @@ box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 margin:0 auto;
 margin-top:10px;
 margin-bottom:10px;
-word-break:break-all;
+word-break:break-word;
 
 h1{
     padding:20px 0 ;
