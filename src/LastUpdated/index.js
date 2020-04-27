8ccfa02b915f54e2ea80aacc255d6c8a06c72ebb
Delshad <Delshadk1@gmail.com>
2020-04-28 00:17:47 +0200

    update casino comeon

--- src/LastUpdated/index.js
+++ src/LastUpdated/index.js
@@ -1,11 +1,7 @@
-import React from "react"
-
-
+import React from 'react';
 
 const LatestUpdate = () => {
+  return <p className="update-text">Senast uppdaterad: 28 April 2020</p>;
+};
 
-    return (<p className="update-text">Senast uppdaterad: 23 Mars 2020</p>)
-
-}
-
-export default LatestUpdate
+export default LatestUpdate;
