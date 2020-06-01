938477135b2f433da50f9ba27308505527179028
Delshad <Delshadk1@gmail.com>
2020-06-01 22:17:38 +0200

    update and news add

--- src/App/index.js
+++ src/App/index.js
@@ -1,136 +1,240 @@
 import React, { useEffect, useState } from 'react';
-import Casinolist from "../data/casinoList.json";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Startpage from "../StartPage/index";
-import NavBarApp from "../Navbar/index";
-import Recension from "../Recension/index"
-import Footer from "../Footer/index"
-import Esport from "../Esport/index"
-import { Container, Alert } from "reactstrap"
-import CasinoMedFaktura from "../CasinoMedFaktura/index"
+import Casinolist from '../data/casinoList.json';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Startpage from '../StartPage/index';
+import NavBarApp from '../Navbar/index';
+import Recension from '../Recension/index';
+import Footer from '../Footer/index';
+import Esport from '../Esport/index';
+import { Container, Alert } from 'reactstrap';
+import CasinoMedFaktura from '../CasinoMedFaktura/index';
 import RedirectComp from '../RedirectComp/index';
-import Sports from "../Sports/index"
-import Terms from "../Terms/index"
-import Policy from "../Policy/index"
-import NotFoundPage from "../NotFoundPage/index"
-import Top5 from "../Blogg/Top5"
-import TopButton from "../TopButton/index"
-import Blogg from "../Blogg/index"
-import MinstaInsCasino from "../Blogg/MinstaInsCasino/index"
-import NyaCasino from "../Blogg/NyaCasinon/index"
-import NyaSlots from "../Blogg/NyaSlots/index"
-import TrustlyCasino from "../CasinonMed/Trustly"
-import SwishCasino from "../CasinonMed/Swish"
-import PaysafeCasino from "../CasinonMed/Paysafe"
-import "./style.scss"
-import sweflag from "../images/sweflag.png"
+import Sports from '../Sports/index';
+import Terms from '../Terms/index';
+import Policy from '../Policy/index';
+import NotFoundPage from '../NotFoundPage/index';
+import Top5 from '../Blogg/Top5';
+import TopButton from '../TopButton/index';
+import Blogg from '../Blogg/index';
+import MinstaInsCasino from '../Blogg/MinstaInsCasino/index';
+import NyaCasino from '../Blogg/NyaCasinon/index';
+import NyaSlots from '../Blogg/NyaSlots/index';
+import TrustlyCasino from '../CasinonMed/Trustly';
+import SwishCasino from '../CasinonMed/Swish';
+import PaysafeCasino from '../CasinonMed/Paysafe';
+import './style.scss';
+import sweflag from '../images/sweflag.png';
 import Casinobonuscomp from '../CasinonMed/Casinobonusar/index.js';
-import News from "../CasinoBlogg/News"
+import News from '../CasinoBlogg/News';
 import NewsSection from '../CasinoBlogg/index.js';
 
-
-
-
-
 const App = () => {
+  const [adBlockEnabled, setAdBlock] = useState(false);
+  const [visible, setVisible] = useState(true);
 
-    const [adBlockEnabled, setAdBlock] = useState(false);
-    const [visible, setVisible] = useState(true);
+  useEffect(() => {
+    let testAd2 = document.createElement('div');
+    testAd2.innerHTML = '&nbsp;';
+    testAd2.className = 'adsBanner ad ads doubleclick';
 
-    useEffect(() => {
-        let testAd2 = document.createElement('div');
-        testAd2.innerHTML = '&nbsp;'
-        testAd2.className = 'adsBanner ad ads doubleclick';
-
-        let testAd = document.createElement('div');
-        testAd.innerHTML = '&nbsp;';
-        testAd.className = 'adsbox';
-        document.body.appendChild(testAd);
-        document.body.appendChild(testAd2);
-        if (testAd.offsetHeight === 0 || testAd2.clientHeight === 0) {
-            setAdBlock(true)
-        }
-        testAd.remove();
-        testAd2.remove()
-
-
-    }, [])
-
-    const onDismiss = () => {
-        setVisible(false)
+    let testAd = document.createElement('div');
+    testAd.innerHTML = '&nbsp;';
+    testAd.className = 'adsbox';
+    document.body.appendChild(testAd);
+    document.body.appendChild(testAd2);
+    if (testAd.offsetHeight === 0 || testAd2.clientHeight === 0) {
+      setAdBlock(true);
     }
+    testAd.remove();
+    testAd2.remove();
+  }, []);
 
+  const onDismiss = () => {
+    setVisible(false);
+  };
 
-    const Go = () => {
-        return <h6>Redirecting....</h6>
-    }
+  const Go = () => {
+    return <h6>Redirecting....</h6>;
+  };
 
+  return (
+    <Router>
+      <div className="App">
+        <TopButton />
+        {window.location.pathname.includes('/redirect') ? '' : <NavBarApp />}
 
-    return (
-        <Router>
-
-            <div className="App">
-
-                <TopButton />
-                {window.location.pathname.includes('/redirect') ? '' : <NavBarApp />}
-
-
-
-                <Container className="app-container">
-
-
-                    <Alert
-                        color="success"
-                        isOpen={visible}
-                        toggle={onDismiss}
-                    >
-                        <div className="swe-license">
-                            <p className="swe-lic-text">
-                                Vi listar endast casinon med svensk spellicens!
-            </p>
-                            <img alt="swe-flag" className="swe-flag" src={sweflag} />
-                        </div>
-                    </Alert>
-
-
-
-                    {adBlockEnabled ? <span className="blocker">OBS! Du har en Adblocker aktiverad. Sidan kan bete sig lite annorlunda än vanligt. </span> : ''}
-                    <Switch>
-                        <Route exact path="/" render={(props) => (<Startpage start={Casinolist} isBlocked={adBlockEnabled} {...props} />)} />
-                        <Route exact path="/recension/:casinotitle" render={(props) => (<Recension list={Casinolist} isBlocked={adBlockEnabled} {...props} />)} />
-                        <Route path="/esportbetting" render={(props) => (<Esport list={Casinolist} isBlocked={adBlockEnabled} {...props} />)} />
-                        <Route path="/faktura-casino" render={(props) => (<CasinoMedFaktura list={Casinolist} isBlocked={adBlockEnabled}  {...props} />)} />
-                        <Route path="/casino-trustly" render={(props) => (<TrustlyCasino list={Casinolist} isBlocked={adBlockEnabled}  {...props} />)} />
-                        <Route path="/casino-bonus" render={(props) => (<Casinobonuscomp list={Casinolist} isBlocked={adBlockEnabled}  {...props} />)} />
-                        <Route path="/casino-swish" render={(props) => (<SwishCasino list={Casinolist} isBlocked={adBlockEnabled}  {...props} />)} />
-                        <Route path="/casino-paysafecard" render={(props) => (<PaysafeCasino list={Casinolist} isBlocked={adBlockEnabled}  {...props} />)} />
-                        <Route path="/Redirect/:casinotitle" render={(props) => (<RedirectComp list={Casinolist}  {...props} />)} />
-                        <Route path="/odds" render={(props) => (<Sports list={Casinolist} isBlocked={adBlockEnabled} {...props} />)} />
-                        <Route path="/kampanjer-casino-2020" component={Blogg} />
-                        <Route path="/terms" component={Terms} />
-                        <Route path="/policy" component={Policy} />
-
-
-                        {/* BLOGG */}
-                        <Route path="/5-basta-casinon-2020" render={(props) => (<Top5 list={Casinolist} isBlocked={adBlockEnabled} {...props} />)} />
-                        <Route path="/minsta-insattning-casino" render={(props) => (<MinstaInsCasino list={Casinolist} isBlocked={adBlockEnabled} {...props} />)} />
-                        <Route path="/nya-casino-2020" render={(props) => (<NyaCasino list={Casinolist} isBlocked={adBlockEnabled} {...props} />)} />
-                        <Route path="/nya-slots-2020" render={(props) => (<NyaSlots list={Casinolist} isBlocked={adBlockEnabled} {...props} />)} />
-                        <Route path="/casino-nyheter/" render={(props) => (<NewsSection  {...props} />)} />
-                        <Route path="/nyheter/:news" render={(props) => (<News list={Casinolist} {...props} />)} />
-                        <Route path="/Go/:casinotitle" render={(props) => (<Go />)} />
-                        <Route path="/*" component={NotFoundPage} />
-                    </Switch>
-
-
-                </Container>
-                <Footer />
+        <Container className="app-container">
+          <Alert color="success" isOpen={visible} toggle={onDismiss}>
+            <div className="swe-license">
+              <p className="swe-lic-text">
+                Vi listar endast casinon med svensk spellicens!
+              </p>
+              <img alt="swe-flag" className="swe-flag" src={sweflag} />
             </div>
+          </Alert>
 
+          {adBlockEnabled ? (
+            <span className="blocker">
+              OBS! Du har en Adblocker aktiverad. Sidan kan bete sig lite
+              annorlunda än vanligt.{' '}
+            </span>
+          ) : (
+            ''
+          )}
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <Startpage
+                  start={Casinolist}
+                  isBlocked={adBlockEnabled}
+                  {...props}
+                />
+              )}
+            />
+            <Route
+              exact
+              path="/recension/:casinotitle"
+              render={props => (
+                <Recension
+                  list={Casinolist}
+                  isBlocked={adBlockEnabled}
+                  {...props}
+                />
+              )}
+            />
+            <Route
+              path="/esportbetting"
+              render={props => (
+                <Esport
+                  list={Casinolist}
+                  isBlocked={adBlockEnabled}
+                  {...props}
+                />
+              )}
+            />
+            <Route
+              path="/faktura-casino"
+              render={props => (
+                <CasinoMedFaktura
+                  list={Casinolist}
+                  isBlocked={adBlockEnabled}
+                  {...props}
+                />
+              )}
+            />
+            <Route
+              path="/casino-trustly"
+              render={props => (
+                <TrustlyCasino
+                  list={Casinolist}
+                  isBlocked={adBlockEnabled}
+                  {...props}
+                />
+              )}
+            />
+            <Route
+              path="/casino-bonus"
+              render={props => (
+                <Casinobonuscomp
+                  list={Casinolist}
+                  isBlocked={adBlockEnabled}
+                  {...props}
+                />
+              )}
+            />
+            <Route
+              path="/casino-swish"
+              render={props => (
+                <SwishCasino
+                  list={Casinolist}
+                  isBlocked={adBlockEnabled}
+                  {...props}
+                />
+              )}
+            />
+            <Route
+              path="/casino-paysafecard"
+              render={props => (
+                <PaysafeCasino
+                  list={Casinolist}
+                  isBlocked={adBlockEnabled}
+                  {...props}
+                />
+              )}
+            />
+            <Route
+              path="/Redirect/:casinotitle"
+              render={props => <RedirectComp list={Casinolist} {...props} />}
+            />
+            <Route
+              path="/odds"
+              render={props => (
+                <Sports
+                  list={Casinolist}
+                  isBlocked={adBlockEnabled}
+                  {...props}
+                />
+              )}
+            />
+            <Route path="/kampanjer-casino-2020" component={Blogg} />
+            <Route path="/terms" component={Terms} />
+            <Route path="/policy" component={Policy} />
 
-        </Router>
-    );
-}
+            {/* BLOGG */}
+            <Route
+              path="/5-basta-casinon-2020"
+              render={props => (
+                <Top5 list={Casinolist} isBlocked={adBlockEnabled} {...props} />
+              )}
+            />
+            <Route
+              path="/minsta-insattning-casino"
+              render={props => (
+                <MinstaInsCasino
+                  list={Casinolist}
+                  isBlocked={adBlockEnabled}
+                  {...props}
+                />
+              )}
+            />
+            <Route
+              path="/nya-casino-2020"
+              render={props => (
+                <NyaCasino
+                  list={Casinolist}
+                  isBlocked={adBlockEnabled}
+                  {...props}
+                />
+              )}
+            />
+            <Route
+              path="/nya-slots-2020"
+              render={props => (
+                <NyaSlots
+                  list={Casinolist}
+                  isBlocked={adBlockEnabled}
+                  {...props}
+                />
+              )}
+            />
+            <Route
+              path="/casino-nyheter/"
+              render={props => <NewsSection {...props} />}
+            />
+            <Route
+              path="/nyheter/:news"
+              render={props => <News list={Casinolist} {...props} />}
+            />
+            <Route path="/Go/:casinotitle" render={props => <Go />} />
+            <Route path="/*" component={NotFoundPage} />
+          </Switch>
+        </Container>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
 
-
-export default App
+export default App;
--- src/CasinoBlogg/index.js
+++ src/CasinoBlogg/index.js
@@ -1,96 +1,134 @@
-import React from "react";
-import { Container } from "reactstrap"
-import { NavLink } from "react-router-dom";
-import { Helmet } from "react-helmet";
-import styled from "styled-components"
-import './style.scss'
-
-
+import React from 'react';
+import { Container } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+import styled from 'styled-components';
+import './style.scss';
 
 const BloggSection = styled.section`
-display: flex;
- justify-content:space-evenly;
- flex-wrap:wrap;
-`
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+`;
 
+const NewsSection = props => {
+  const Kampanj = props => {
+    let { text, bild, publicerad, link, title, alttxt } = props;
+    return (
+      <section className="kampanj">
+        <NavLink to={{ pathname: `/nyheter/${link}`, match: `${link}` }}>
+          <img
+            srcSet={`${bild} 300w,${bild} 200w`}
+            sizes="(max-width: 500px) 200px,400px"
+            src={bild}
+            alt={alttxt}
+          />
+        </NavLink>
+        <h5>{title}</h5>
+        <p>{text}...</p>
 
-const NewsSection = (props) => {
+        <NavLink
+          className="readmore-blogg"
+          to={{
+            pathname: `/nyheter/${link}`,
+            match: `${link}`,
+          }}
+        >
+          Läs mer
+        </NavLink>
 
+        <small>{publicerad}</small>
+      </section>
+    );
+  };
 
+  return (
+    <Container>
+      <Helmet>
+        <title>
+          Casino Nyheter 2020 » Nyheter Inom Svenska Casinobranschen | Casinoniz
+        </title>
+        <link rel="canonical" href="https://www.casinoniz.se/casino-nyheter" />
+        <meta
+          name="description"
+          content="Senaste svenska casino nyheter 2020 ➼ Här lägger vi på Casinoniz ut några nyheter om vad som sker inom casinobranschen. Vill du hålla dig uppdaterad om vilka casinon som t.ex. blir av med sin svenska spellicens eller liknande så hittar du det här."
+        />
+        <meta
+          name="keywords"
+          content="Casino, Casinobonusar, kasino, kasinobonusar,välkomstbonus, 2019,2020, casinonyheter,nyheter om casino, casinoblogg, kasinonyheter,casinonews"
+        />
 
-  const Kampanj = (props) => {
-    let { text, bild, publicerad, link, title, alttxt } = props
-    return (<section className="kampanj">
-      <NavLink to={{ pathname: `/nyheter/${link}`, match: `${link}` }}><img
-        srcSet={`${bild} 300w,${bild} 200w`}
-        sizes="(max-width: 500px) 200px,400px"
-        src={bild} alt={alttxt} /></NavLink>
-      <h5>{title}</h5>
-      <p>{text}...</p>
+        <meta property="og:title" content="Nyheter om casinobranschen 2020. " />
+        <meta
+          property="og:description"
+          content="Vill du hålla dig uppdaterad om vilka casinon som t.ex. blir av med sin svenska spellicens eller liknande så hittar du det här."
+        />
+        <meta
+          property="og:image"
+          content="https://www.casinoniz.se/images/ninja-min.png"
+        />
+        <meta property="og:url" content="https://www.casinoniz.se/nyheter" />
+      </Helmet>
 
-      <NavLink className="readmore-blogg" to={{
-        pathname: `/nyheter/${link}`, match: `${link}`
-      }}>Läs mer</NavLink>
+      <section className="blogg">
+        <h1 className="banners">Casino nyheter 2020 </h1>
+        <p>
+          Vill du hålla dig uppdaterad om nyheter inom casinobranschen ? Här har
+          vi en liten nyhetssida där vi släpper intressanta nyheter om vad som
+          händer inom casinobranschen. Hoppas att det vi skriver ut här får dig
+          som läsare att finna det intressant :){' '}
+        </p>
 
-      <small>{publicerad}</small>
-    </section>)
-  }
+        <h2>Casino Nyheter 2020</h2>
+        <BloggSection>
+          <Kampanj
+            title="Casinon som stänger i Sverige"
+            text="Flera svenska välkända casinon har valt att stänga ner sin verksamhet i Sverige. Dem har valt att inte ansöka om svensk spellicens för närvarande och stänger därmed registreringen för svenska spelare"
+            bild="/images/closed.png"
+            publicerad="1 Juni 2020"
+            link="flera-casino-stanger"
+            alttxt="svenskacasinostanger"
+          />
 
+          <Kampanj
+            title="Highroller - Casinot stänger verksamhet i Sverige"
+            text="Zecure Gaming Limited är bolaget som driver Highroller. Bolaget har valt att stänga ner Highroller för svenska spelare och hänvisar istället till deras systercasino som är Casinoroom.se"
+            bild="/images/highrollerclose.png"
+            publicerad="16 februari 2020"
+            link="highroller-stanger"
+            alttxt="highrollerclose"
+          />
 
-  return (<Container>
-    <Helmet>
-      <title>Casino Nyheter 2020 » Nyheter Inom Svenska Casinobranschen | Casinoniz</title>
-      <link rel="canonical" href="https://www.casinoniz.se/casino-nyheter" />
-      <meta
-        name="description"
-        content="Senaste svenska casino nyheter 2020 ➼ Här lägger vi på Casinoniz ut några nyheter om vad som sker inom casinobranschen. Vill du hålla dig uppdaterad om vilka casinon som t.ex. blir av med sin svenska spellicens eller liknande så hittar du det här."
-      />
-      <meta
-        name="keywords"
-        content="Casino, Casinobonusar, kasino, kasinobonusar,välkomstbonus, 2019,2020, casinonyheter,nyheter om casino, casinoblogg, kasinonyheter,casinonews"
-      />
+          <Kampanj
+            title="Nytt casino lanseras 2020 - förhoppningsvis under februari"
+            text="Nu är det dags för ett nytt casino 2020. Casinot är SlotV.com. Casinot grundades 2017 och är aktiva i över 20 länder just nu. Bolaget bakom casinot är Avento MT Limited."
+            bild="/images/slotvimg.png"
+            publicerad="1 februari 2020"
+            link="slotv-nyttcasino"
+            alttxt="slotvlogo"
+          />
 
-      <meta property="og:title" content="Nyheter om casinobranschen 2020. " />
-      <meta property="og:description" content="Vill du hålla dig uppdaterad om vilka casinon som t.ex. blir av med sin svenska spellicens eller liknande så hittar du det här." />
-      <meta property="og:image" content="https://www.casinoniz.se/images/ninja-min.png" />
-      <meta property="og:url" content="https://www.casinoniz.se/nyheter" />
+          <Kampanj
+            title="Slotsons stänger ner sin verksamhet"
+            text="Viral Interactive Limited har beslutat att stänga ner onlinecasinot Slotsons.com. Från och med 1 februari 2020 så stänger man registreringar hos casinot. Alla aktiva spelare kommer tillåtas att ta ut sitt saldo under en period"
+            bild="/images/slotson.png"
+            publicerad="31 januari 2020"
+            link="slotsons-laggerner"
+            alttxt="slotsonlogo"
+          />
 
-    </Helmet>
+          <Kampanj
+            title="Ninja casino kommer tillbaka?"
+            text="Det kanske har märkts att det varit ganska tyst från det populära onlinecasinot Ninjacasino.se. Casinot blev av med sin svenska spellicens den 17 juni 2019 sedan spelinspektionen upptäckt allvarliga brister gällande arbetet mot penningtvätt och spelansvar"
+            bild="/images/mininja.png"
+            publicerad="29 januari 2020"
+            link="ninjacasino-aterkomst"
+            alttxt="ninjacasinologo"
+          />
+        </BloggSection>
+      </section>
+    </Container>
+  );
+};
 
-
-    <section className="blogg">
-
-      <h1 className="banners">Casino nyheter 2020 </h1>
-      <p>Vill du hålla dig uppdaterad om nyheter inom casinobranschen ? Här har vi en liten nyhetssida där vi släpper intressanta nyheter om vad som händer inom casinobranschen.  Hoppas att det vi skriver ut här får dig som läsare att finna det intressant :) </p>
-
-
-      <h2>Casino Nyheter 2020</h2>
-      <BloggSection>
-
-
-
-
-        <Kampanj title="Highroller - Casinot stänger verksamhet i Sverige" text='Zecure Gaming Limited är bolaget som driver Highroller. Bolaget har valt att stänga ner Highroller för svenska spelare och hänvisar istället till deras systercasino som är Casinoroom.se' bild="/images/highrollerclose.png" publicerad="16 februari 2020" link="highroller-stanger" alttxt="highrollerclose" />
-
-        <Kampanj title="Nytt casino lanseras 2020 - förhoppningsvis under februari" text='Nu är det dags för ett nytt casino 2020. Casinot är SlotV.com. Casinot grundades 2017 och är aktiva i över 20 länder just nu. Bolaget bakom casinot är Avento MT Limited.' bild="/images/slotvimg.png" publicerad="1 februari 2020" link="slotv-nyttcasino" alttxt="slotvlogo" />
-
-        <Kampanj title="Slotsons stänger ner sin verksamhet" text='Viral Interactive Limited har beslutat att stänga ner onlinecasinot Slotsons.com. Från och med 1 februari 2020 så stänger man registreringar hos casinot. Alla aktiva spelare kommer tillåtas att ta ut sitt saldo under en period' bild="/images/slotson.png" publicerad="31 januari 2020" link="slotsons-laggerner" alttxt="slotsonlogo" />
-
-        <Kampanj title="Ninja casino kommer tillbaka?" text='Det kanske har märkts att det varit ganska tyst från det populära onlinecasinot Ninjacasino.se. Casinot blev av med sin svenska spellicens den 17 juni 2019 sedan spelinspektionen upptäckt allvarliga brister gällande arbetet mot penningtvätt och spelansvar' bild="/images/mininja.png" publicerad="29 januari 2020" link="ninjacasino-aterkomst" alttxt="ninjacasinologo" />
-
-
-
-
-
-
-      </BloggSection>
-
-
-    </section>
-  </Container>
-
-
-  )
-}
-
-export default NewsSection
+export default NewsSection;
