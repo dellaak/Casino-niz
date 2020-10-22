0bac2a1eab3044536cddbc0a8d4f754c8720afec
delshad kogi <delshad@MacBook-Pro-som-tillhor-delshad.local>
2020-10-23 00:13:46 +0200

    update

--- src/LastUpdated/index.js
+++ src/LastUpdated/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LatestUpdate = () => {
-  return <p className="update-text">Senast uppdaterad: 22 Oktober 2020</p>;
+  return <p className="update-text">Senast uppdaterad: 23 Oktober 2020</p>;
 };
 
 export default LatestUpdate;
