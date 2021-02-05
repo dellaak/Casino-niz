1065e1c62d2f1cc0b2650296132bcea30431d701
delshad kogi <delshad@MacBook-Pro-som-tillhor-delshad.local>
2021-02-06 00:31:55 +0100

    update and remove turbico

--- src/LastUpdated/index.js
+++ src/LastUpdated/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LatestUpdate = () => {
-  return <p className="update-text">Senast uppdaterad: 31 Januari 2021</p>;
+  return <p className="update-text">Senast uppdaterad: 6 Februari 2021</p>;
 };
 
 export default LatestUpdate;
